644aab651df22b6b968a9b4e2dd12ffba529f582
Bryon <30876973+nukeguard@users.noreply.github.com>
2021-04-25 04:37:06 -0400

    Update DroneBay.cs

--- Data/Scripts/WeaponThread/DroneBay.cs
+++ Data/Scripts/WeaponThread/DroneBay.cs
@@ -25,7 +25,7 @@ namespace WeaponThread
                         MuzzlePartId = "MissileTurretBarrels",
                         AzimuthPartId = "MissileTurretBase1",
                         ElevationPartId = "MissileTurretBarrels", 
-                        DurabilityMod = 1.0f,
+                        DurabilityMod = 0.1f,
                         IconName = "Plac_empty.dds" 
                     },
                 },
